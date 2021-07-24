efbdbf2c7fe51d2d6ff544565c18d011d893c1a3
Ifeanyi <alichiifeanyi@yahoo.com>
2021-07-24 22:35:45 +0100

    initial commit

---
+++ lib/screens/tasks_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class TasksScreen extends StatelessWidget {
+  const TasksScreen({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlueAccent,
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            Icon(Icons.list),
+            Text('Todoey')
+          ],
+        ),
+      ),
+    );
+  }
+}
