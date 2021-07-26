81d314fc8c6714d6b2d2f58096c652207efd5e66
Ifeanyi <alichiifeanyi@yahoo.com>
2021-07-26 10:00:02 +0100

    edits

--- lib/screens/add_task_screen.dart
+++ lib/screens/add_task_screen.dart
@@ -1,12 +1,63 @@
 import 'package:flutter/material.dart';
 
 class AddTaskScreen extends StatelessWidget {
-  const AddTaskScreen({ Key? key }) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      color: Color(0xFF757575),
+      child: Container(
+        padding: EdgeInsets.only(
+          left: 30.0,
+          right: 30.0,
+        ),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20.0),
+            topRight: Radius.circular(20.0),
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Add Task',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.lightBlueAccent,
+                fontSize: 30.0,
+              ),
+            ),
+            TextField(
+              autofocus: true,
+              textAlign: TextAlign.center,
+              decoration: InputDecoration(
+                border: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    style: BorderStyle.solid,
+                    color: Colors.lightBlueAccent,
+                    width: 10.0,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('Add'),
+              style: ButtonStyle(
+                backgroundColor:
+                    MaterialStateProperty.all<Color>(Colors.lightBlueAccent),
+                padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+                    EdgeInsets.symmetric(vertical: 20.0)),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
