e7ad7bbd2fd92e7bda5e4c32ccde86a2dd984f8c
Ifeanyi <alichiifeanyi@yahoo.com>
2021-09-29 16:23:34 +0100

    edit

--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -21,7 +21,7 @@ class _TasksListState extends State<TasksList> {
         return TaskTile(
             taskTitle: tasks[index].name,
             isChecked: tasks[index].isDone,
-            checkboxCallback: (bool checkboxState) {
+            checkboxCallback: (checkboxState) {
               setState(() {
                 tasks[index].toggleDone();
               });
